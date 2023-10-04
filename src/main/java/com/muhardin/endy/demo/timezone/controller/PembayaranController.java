6815ab5c993b5e9bf00fe743752c1202b3193ceb
Endy Muhardin <endy.muhardin@gmail.com>
2023-10-04 21:48:38 +0700

    menampilkan data pembayaran

--- src/main/java/com/muhardin/endy/demo/timezone/controller/PembayaranController.java
+++ src/main/java/com/muhardin/endy/demo/timezone/controller/PembayaranController.java
@@ -2,6 +2,7 @@ package com.muhardin.endy.demo.timezone.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,5 +20,10 @@ public class PembayaranController {
     @ResponseStatus(HttpStatus.CREATED)
     public void simpan(@RequestBody Pembayaran pembayaran) {
         pembayaranDao.save(pembayaran);
+    }
+
+    @GetMapping("/api/pembayaran/")
+    public Iterable<Pembayaran> dataPembayaran(){
+        return pembayaranDao.findAll();
     }
 }
