b2f59de789637567c81be048270943e89ad5e46b
Endy Muhardin <endy.muhardin@gmail.com>
2023-10-04 21:11:12 +0700

    starter project springboot

---
+++ src/main/java/com/muhardin/endy/demo/timezone/DemoTimezoneApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.demo.timezone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTimezoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTimezoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muhardin/endy/demo/timezone/DemoTimezoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.demo.timezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTimezoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
